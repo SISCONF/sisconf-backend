628c9f8dbfd18be0b9716eda757cf7f6b114a9a5
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-12-03 13:13:20 -0300

    chore: project initial setup

---
+++ src/main/java/br/ifrn/edu/sisconf/SisconfApplication.java
@@ -0,0 +1,13 @@
+package br.ifrn.edu.sisconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisconfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisconfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifrn/edu/sisconf/SisconfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifrn.edu.sisconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisconfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
