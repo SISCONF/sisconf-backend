dbb6b6db30a1fb921c3e6e347672b8dd98313903
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-12-09 09:46:36 -0300

    feat: adds exception handling in all Food entity's routes.

--- src/main/java/br/ifrn/edu/sisconf/service/FoodService.java
+++ src/main/java/br/ifrn/edu/sisconf/service/FoodService.java
@@ -3,6 +3,7 @@ package br.ifrn.edu.sisconf.service;
 import br.ifrn.edu.sisconf.domain.Food;
 import br.ifrn.edu.sisconf.domain.dtos.FoodRequestDTO;
 import br.ifrn.edu.sisconf.domain.dtos.FoodResponseDTO;
+import br.ifrn.edu.sisconf.exception.BusinessException;
 import br.ifrn.edu.sisconf.exception.ResourceNotFoundException;
 import br.ifrn.edu.sisconf.mapper.FoodMapper;
 import br.ifrn.edu.sisconf.repository.FoodRepository;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class FoodService {
@@ -22,6 +22,12 @@ public class FoodService {
     private FoodMapper mapper;
 
     public FoodResponseDTO createFood(FoodRequestDTO createFoodDto) {
+        if (createFoodDto.getUnit_price() != null && createFoodDto.getUnit_price().signum() != 1) {
+            throw new BusinessException("O preço unitário não pode ser 0, nem negativo.");
+        }
+        if (createFoodDto.getName() != null && createFoodDto.getName().isEmpty()) {
+            throw new BusinessException("O campo nome não pode ser um texto vazio.");
+        }
         var food = mapper.toEntity(createFoodDto);
         foodRepository.save(food);
         return mapper.toResponseDTO(food);
@@ -34,17 +40,14 @@ public class FoodService {
 
     public void delete(Long id) {
         if (!foodRepository.existsById(id)) {
-            throw new ResourceNotFoundException("Comida não encontrada");
+            throw new ResourceNotFoundException("Comida não encontrada.");
         }
         foodRepository.deleteById(id);
     }
 
     public FoodResponseDTO getFood(Long id) {
-        Optional<Food> foundFood = foodRepository.findById(id);
-        Food food = foundFood.get();
-        if (food == null) {
-            throw new ResourceNotFoundException("Esta comida não está registrada no banco");
-        }
+        Food food = foodRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comida não encontrada."));
         return mapper.toResponseDTO(food);
 
     }
@@ -52,6 +55,12 @@ public class FoodService {
     public FoodResponseDTO update(Long id, FoodRequestDTO foodDto) {
         Food food = foodRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Comida não econtrada."));
+        if (foodDto.getUnit_price() != null && foodDto.getUnit_price().signum() != 1) {
+            throw new BusinessException("O preço unitário não pode ser 0, nem negativo.");
+        }
+        if (foodDto.getName() != null && foodDto.getName().isEmpty()) {
+            throw new BusinessException("O campo nome não pode ser um texto vazio.");
+        }
         mapper.updateEntityFromDTO(foodDto, food);
         var updatedFood = foodRepository.save(food);
 
