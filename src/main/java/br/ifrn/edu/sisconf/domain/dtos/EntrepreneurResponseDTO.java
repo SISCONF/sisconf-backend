86bbe4a7798525201ccd1e2b491ececcf85d1e77
fernandes.gentilalysonjac@gmail.com <fernandes.gentilalysonjac@gmail.com>
2024-12-24 17:08:55 -0300

    feat: adds dtos and mapper for entrepreneur

---
+++ src/main/java/br/ifrn/edu/sisconf/domain/dtos/EntrepreneurResponseDTO.java
@@ -0,0 +1,17 @@
+package br.ifrn.edu.sisconf.domain.dtos;
+
+import br.ifrn.edu.sisconf.domain.Stock;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntrepreneurResponseDTO {
+    private PersonResponseDTO person;
+    private String businessName;
+    private Stock stock;
+}
---
+++ src/main/java/br/ifrn/edu/sisconf/mapper/EntrepreneurMapper.java
@@ -0,0 +1,18 @@
+package br.ifrn.edu.sisconf.mapper;
+
+import br.ifrn.edu.sisconf.domain.Entrepreneur;
+import br.ifrn.edu.sisconf.domain.dtos.EntrepreneurCreateRequestDTO;
+import br.ifrn.edu.sisconf.domain.dtos.EntrepreneurResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface EntrepreneurMapper {
+    @Mapping(source = "person.address.city", target = "person.address.city.id")
+    @Mapping(target = "stock", ignore = true)
+    Entrepreneur toEntity(EntrepreneurCreateRequestDTO entrepreneurCreateRequestDTO);
+
+    @Mapping(source = "person.address.city.id", target = "person.address.city")
+    EntrepreneurResponseDTO toResponseDTO(Entrepreneur entrepreneur);
+}
