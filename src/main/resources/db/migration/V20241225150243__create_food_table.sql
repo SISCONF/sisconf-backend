17bdb1f142f244528c7036db37dc419b9e0f0ced
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-12-26 20:27:39 -0300

    Revert "raw: adds food table creation migration."
    This reverts commit fde6322ae42f042811aa1d245a7c61e8b6643930.

--- src/main/resources/db/migration/V20241225150243__create_food_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS food(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    category VARCHAR(9),
-    unit_price DECIMAL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
