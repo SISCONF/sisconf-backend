e9f3a54970ccedf62a4696f45c3d5dddf7a54172
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-12-28 20:28:50 -0300

    refactor: removes @Data decorator from Stock entity; raw: adds stock_food table migration file.

--- src/main/java/br/ifrn/edu/sisconf/domain/Stock.java
+++ src/main/java/br/ifrn/edu/sisconf/domain/Stock.java
@@ -1,13 +1,11 @@
 package br.ifrn.edu.sisconf.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @NoArgsConstructor
 @Entity
 @Table(name = "stock")
