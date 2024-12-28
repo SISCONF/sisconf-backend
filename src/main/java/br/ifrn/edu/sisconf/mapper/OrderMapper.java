477f9177a47bd4d0d5734cc452b2681e2ab9ed42
Henrique Eduardo <heduardocr7@gmail.com>
2024-12-28 16:26:38 -0300

    feat: create OrderMapper for conversion between entity and DTO

--- src/main/java/br/ifrn/edu/sisconf/mapper/OrderMapper.java
+++ src/main/java/br/ifrn/edu/sisconf/mapper/OrderMapper.java
@@ -3,17 +3,16 @@ package br.ifrn.edu.sisconf.mapper;
 import br.ifrn.edu.sisconf.domain.Order;
 import br.ifrn.edu.sisconf.domain.dtos.OrderRequestDTO;
 import br.ifrn.edu.sisconf.domain.dtos.OrderResponseDTO;
+import br.ifrn.edu.sisconf.domain.dtos.OrderUpdateRequestDTO;
 import org.mapstruct.*;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface OrderMapper {
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    Order toEntity(OrderRequestDTO dto);
+    Order toEntity(OrderRequestDTO orderRequestDTO);
 
+    @Mapping(target = "customerId", source = "customer.id")
     OrderResponseDTO toResponseDTO(Order order);
 
     List<OrderResponseDTO> toDTOList(List<Order> orders);
@@ -22,6 +21,5 @@ public interface OrderMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
-
-    void updateEntityFromDTO(OrderRequestDTO dto, @MappingTarget Order order);
+    void updateEntityFromDTO(OrderUpdateRequestDTO orderUpdateRequestDTO, @MappingTarget Order order);
 }
