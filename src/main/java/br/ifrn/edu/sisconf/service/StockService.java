2de9eea72d13b7fce4599f2b3a5f7af266ece17d
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-12-29 17:32:46 -0300

    feat: adds StockController.

--- src/main/java/br/ifrn/edu/sisconf/service/StockService.java
+++ src/main/java/br/ifrn/edu/sisconf/service/StockService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import br.ifrn.edu.sisconf.domain.Entrepreneur;
 import br.ifrn.edu.sisconf.domain.Stock;
 import br.ifrn.edu.sisconf.domain.dtos.StockResponseDTO;
+import br.ifrn.edu.sisconf.exception.ResourceNotFoundException;
 import br.ifrn.edu.sisconf.mapper.StockMapper;
+import br.ifrn.edu.sisconf.repository.EntrepreneurRepository;
 import br.ifrn.edu.sisconf.repository.StockRepository;
 
 @Service
@@ -16,6 +18,9 @@ public class StockService {
 
     @Autowired
     private StockMapper stockMapper;
+
+    @Autowired
+    private EntrepreneurRepository entrepreneurRepository;
 
     public void save(Entrepreneur entrepreneur) {
         Stock stock = new Stock();
@@ -28,7 +33,8 @@ public class StockService {
         stockRepository.deleteById(stock.getId());
     }
 
-    public StockResponseDTO get(Entrepreneur entrepreneur) {
+    public StockResponseDTO get(Long entrepreneurId) {
+        Entrepreneur entrepreneur = entrepreneurRepository.findById(entrepreneurId).orElseThrow(() -> new ResourceNotFoundException("Este empreendedor não existe"));
         Stock stock = entrepreneur.getStock();
         return stockMapper.toResponseDTO(stock);
     }
