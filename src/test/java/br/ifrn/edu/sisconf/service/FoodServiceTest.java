6c52f9e84f03edb2f1c6474e5f8e88649b05eb30
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-01-01 17:13:06 -0300

    feat: adds test cases for createFood method.

--- src/test/java/br/ifrn/edu/sisconf/service/FoodServiceTest.java
+++ src/test/java/br/ifrn/edu/sisconf/service/FoodServiceTest.java
@@ -1,7 +1,10 @@
 package br.ifrn.edu.sisconf.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
@@ -14,6 +17,7 @@ import org.mockito.MockitoAnnotations;
 
 import br.ifrn.edu.sisconf.domain.Food;
 import br.ifrn.edu.sisconf.domain.dtos.FoodRequestDTO;
+import br.ifrn.edu.sisconf.domain.dtos.FoodResponseDTO;
 import br.ifrn.edu.sisconf.domain.enums.FoodCategory;
 import br.ifrn.edu.sisconf.exception.BusinessException;
 import br.ifrn.edu.sisconf.mapper.FoodMapper;
@@ -96,5 +100,65 @@ public class FoodServiceTest {
         when(foodRepository.existsByNameAndCategoryAndIdNot(foodRequestDTO.getName(), foodRequestDTO.getCategory(), food.getId())).thenReturn(false);
 
         assertDoesNotThrow(() -> foodService.throwIfFoodAlreadyExists(foodRequestDTO, food.getId()));
+    }
+
+    @Test
+    public void createFoodWithSuccessfully() {
+        FoodRequestDTO foodRequestDTO = new FoodRequestDTO();
+        foodRequestDTO.setName("Laranja");
+        foodRequestDTO.setCategory(FoodCategory.FRUIT);
+        foodRequestDTO.setUnitPrice(new BigDecimal(1));
+
+        Food food = new Food();
+        food.setName("Laranja");
+        food.setUnitPrice(new BigDecimal(1));
+        food.setCategory(FoodCategory.FRUIT);
+
+        FoodResponseDTO foodResponseDTO = new FoodResponseDTO();
+        foodResponseDTO.setName("Laranja");
+        foodResponseDTO.setCategory(FoodCategory.FRUIT);
+        foodResponseDTO.setUnitPrice(new BigDecimal(1));
+
+        when(foodRepository.existsByNameAndCategory(foodRequestDTO.getName(), foodRequestDTO.getCategory())).thenReturn(false);
+        when(foodMapper.toEntity(foodRequestDTO)).thenReturn(food);
+        when(foodMapper.toResponseDTO(food)).thenReturn(foodResponseDTO);
+
+        FoodResponseDTO createdFood = foodService.createFood(foodRequestDTO);
+
+        verify(foodRepository).save(food);
+        assertEquals(foodResponseDTO, createdFood);
+    }
+
+    @Test
+    public void failedFoodCreationBecauseOfInvalidPrice() {
+        BigDecimal unitPrice = new BigDecimal(0);
+        FoodRequestDTO foodRequestDTO = new FoodRequestDTO();
+        foodRequestDTO.setUnitPrice(unitPrice);
+        Food food = new Food();
+        food.setUnitPrice(unitPrice);
+
+        when(foodMapper.toEntity(foodRequestDTO)).thenReturn(food);
+
+        assertThrowsExactly(BusinessException.class, () -> foodService.throwIfInvalidUnitPrice(foodRequestDTO));
+
+        verify(foodRepository, never()).save(food);
+    }
+
+    @Test
+    public void failedFoodCreationBecauseOfExistingFood() {
+        FoodRequestDTO foodRequestDTO = new FoodRequestDTO();
+        foodRequestDTO.setName("Laranja");
+        foodRequestDTO.setCategory(FoodCategory.FRUIT);
+
+        Food food = new Food();
+        food.setName("Laranja");
+        food.setCategory(FoodCategory.FRUIT);
+
+        when(foodRepository.existsByNameAndCategory(foodRequestDTO.getName(), foodRequestDTO.getCategory())).thenReturn(true);
+        when(foodMapper.toEntity(foodRequestDTO)).thenReturn(food);
+
+        assertThrowsExactly(BusinessException.class, () -> foodService.throwIfFoodAlreadyExists(foodRequestDTO, null));
+
+        verify(foodRepository, never()).save(food);
     }
 }
