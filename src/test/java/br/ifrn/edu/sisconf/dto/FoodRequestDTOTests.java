9d339ccb82bd19488bf2daa21fb81ce3f4ea4116
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-01-06 15:15:01 -0300

    test: adiciona os dois primeiros testes relativos ao preco unitário de Food.

---
+++ src/test/java/br/ifrn/edu/sisconf/dto/FoodRequestDTOTests.java
@@ -0,0 +1,48 @@
+package br.ifrn.edu.sisconf.dto;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.ifrn.edu.sisconf.domain.dtos.FoodRequestDTO;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+public class FoodRequestDTOTests {
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void foodRequestWithValidUnitPrice() {
+        var foodRequestDTO = new FoodRequestDTO();
+        foodRequestDTO.setUnitPrice(new BigDecimal("0.01"));
+
+        Set<ConstraintViolation<FoodRequestDTO>> violations = validator.validate(foodRequestDTO);
+        assertFalse(violations.stream().anyMatch(violation -> violation.getMessage().equals("O preço unitário é obrigatório")));
+        assertFalse(violations.stream().anyMatch(violation -> violation.getMessage().equals("O preço da comida deve ser maior do que 0")));
+    }
+
+    @Test
+    public void foodRequestWithInvalidUnitPrice() {
+        var foodRequestDTO = new FoodRequestDTO();
+        foodRequestDTO.setUnitPrice(new BigDecimal("0.0"));
+
+        Set<ConstraintViolation<FoodRequestDTO>> violations = validator.validate(foodRequestDTO);
+        assertFalse(violations.stream().anyMatch(violation -> violation.getMessage().equals("O preço unitário é obrigatório")));
+        assertTrue(violations.stream().anyMatch(violation -> violation.getMessage().equals("O preço da comida deve ser maior do que 0")));
+    }
+
+
+}
