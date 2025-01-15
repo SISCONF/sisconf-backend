fa519cdede641c1f84f909680a03b5c3431f54f7
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-01-14 22:06:21 -0300

    test: adiciona todos os testes que se referem somente a Stock (sem StockFood); refactor: renomeia rota de estoques para uma mais semântica e renomeia o método get the StockService para getByEntrepreneurId para ficar mais semântico.

--- src/main/java/br/ifrn/edu/sisconf/controller/StockController.java
+++ src/main/java/br/ifrn/edu/sisconf/controller/StockController.java
@@ -11,13 +11,13 @@ import br.ifrn.edu.sisconf.domain.dtos.StockResponseDTO;
 import br.ifrn.edu.sisconf.service.StockService;
 
 @RestController
-@RequestMapping("/api/entrepreneurs/{entrepreneurId}/stocks")
+@RequestMapping("/api/stocks/entrepreneurs/{entrepreneurId}")
 public class StockController {
     @Autowired
     private StockService stockService;
 
     @GetMapping
     public ResponseEntity<StockResponseDTO> get(@PathVariable Long entrepreneurId) {
-        return ResponseEntity.ok(stockService.get(entrepreneurId));
+        return ResponseEntity.ok(stockService.getByEntrepreneurId(entrepreneurId));
     }
 }
