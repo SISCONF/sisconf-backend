8bf9140e132db1449e0e2e5c0c6c86bbceca0fc0
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-01-27 18:45:22 -0300

    fix: readds custom CustomerResponseDTO constructor because its removal would cause the test to fail

--- src/main/java/br/ifrn/edu/sisconf/domain/dtos/Customer/CustomerResponseDTO.java
+++ src/main/java/br/ifrn/edu/sisconf/domain/dtos/Customer/CustomerResponseDTO.java
@@ -1,5 +1,7 @@
 package br.ifrn.edu.sisconf.domain.dtos.Customer;
 
+import br.ifrn.edu.sisconf.domain.Customer;
+import br.ifrn.edu.sisconf.domain.dtos.AddressResponseDTO;
 import br.ifrn.edu.sisconf.domain.dtos.PersonResponseDTO;
 import br.ifrn.edu.sisconf.domain.enums.CustomerCategory;
 import lombok.*;
@@ -11,4 +13,27 @@ public class CustomerResponseDTO {
     private Long id;
     private PersonResponseDTO person;
     private CustomerCategory category;
+
+    public CustomerResponseDTO(Customer customer) {
+        id = customer.getId();
+        person = new PersonResponseDTO(
+            id, 
+            customer.getPerson().getKeycloakId(), 
+            customer.getPerson().getFirstName(), 
+            customer.getPerson().getLastName(), 
+            customer.getPerson().getEmail(), 
+            customer.getPerson().getCpf(), 
+            customer.getPerson().getCnpj(), 
+            customer.getPerson().getPhone(),
+            new AddressResponseDTO(
+                customer.getPerson().getAddress().getId(), 
+                customer.getPerson().getAddress().getStreet(), 
+                customer.getPerson().getAddress().getZipCode(), 
+                customer.getPerson().getAddress().getNeighbourhood(), 
+                customer.getPerson().getAddress().getNumber(), 
+                customer.getPerson().getAddress().getCity().getId()
+            )
+        );
+        category = customer.getCategory();
+    }
 }
