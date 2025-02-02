a3e3fa6e8457ef872f9517588b1266aa2ffc7918
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-01 22:18:19 -0300

    :sparkles: feat: adds cities list by country state id endpoint

--- src/main/java/br/ifrn/edu/sisconf/controller/CountryStateController.java
+++ src/main/java/br/ifrn/edu/sisconf/controller/CountryStateController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ifrn.edu.sisconf.domain.dtos.CityResponseDTO;
 import br.ifrn.edu.sisconf.domain.dtos.CountryStateResponseDTO;
 import br.ifrn.edu.sisconf.service.CountryStateService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -26,6 +28,14 @@ public class CountryStateController {
     public ResponseEntity<List<CountryStateResponseDTO>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(
             countryStateService.getAll()
+        );
+    }
+
+    @GetMapping("/{id}/cities")
+    @Operation(description = "Lista todas as cidades que são do estado específico")
+    public ResponseEntity<List<CityResponseDTO>> getAllCities(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            countryStateService.getAllCitiesByCountryStateId(id)
         );
     }
 }
--- src/main/java/br/ifrn/edu/sisconf/service/CountryStateService.java
+++ src/main/java/br/ifrn/edu/sisconf/service/CountryStateService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.ifrn.edu.sisconf.domain.dtos.CityResponseDTO;
 import br.ifrn.edu.sisconf.domain.dtos.CountryStateResponseDTO;
+import br.ifrn.edu.sisconf.exception.ResourceNotFoundException;
+import br.ifrn.edu.sisconf.mapper.CityMapper;
 import br.ifrn.edu.sisconf.mapper.CountryStateMapper;
 import br.ifrn.edu.sisconf.repository.CountryStateRepository;
 
@@ -15,10 +18,22 @@ public class CountryStateService {
     private CountryStateMapper countryStateMapper;
 
     @Autowired
+    private CityMapper cityMapper;
+
+    @Autowired
     private CountryStateRepository countryStateRepository;
 
     public List<CountryStateResponseDTO> getAll() {
         var countryStates = countryStateRepository.findAll();
         return countryStateMapper.toDTOList(countryStates);
+    }
+
+    public List<CityResponseDTO> getAllCitiesByCountryStateId(Long countryStateId) {
+        var countryState = countryStateRepository.findById(countryStateId).orElseThrow(() ->
+            new ResourceNotFoundException(
+                "Estado com não encontrado"
+            )
+        );
+        return cityMapper.toDTOList(countryState.getCities());
     }
 }
