0b80748ef62b8294764022cd21f5592ebddc3c3a
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-01 21:59:52 -0300

    :books: docs: adds swagger docs to country state listing

--- src/main/java/br/ifrn/edu/sisconf/controller/CountryStateController.java
+++ src/main/java/br/ifrn/edu/sisconf/controller/CountryStateController.java
@@ -11,14 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.ifrn.edu.sisconf.domain.dtos.CountryStateResponseDTO;
 import br.ifrn.edu.sisconf.service.CountryStateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/country-states")
+@Tag(name = "Country State")
 public class CountryStateController {
     @Autowired
     private CountryStateService countryStateService;
 
     @GetMapping
+    @Operation(description = "Lista todos os estados do Brasil")
     public ResponseEntity<List<CountryStateResponseDTO>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(
             countryStateService.getAll()
