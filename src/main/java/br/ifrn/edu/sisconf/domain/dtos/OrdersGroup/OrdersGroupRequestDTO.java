0fd108766201d0a576782b9c447cecdad317b745
KarlaNunes <karla.julyana1@gmail.com>
2025-02-06 12:57:28 -0300

    feat: add validations in OrdersGroupRequestDTO

--- src/main/java/br/ifrn/edu/sisconf/domain/dtos/OrdersGroup/OrdersGroupRequestDTO.java
+++ src/main/java/br/ifrn/edu/sisconf/domain/dtos/OrdersGroup/OrdersGroupRequestDTO.java
@@ -1,10 +1,9 @@
 package br.ifrn.edu.sisconf.domain.dtos.OrdersGroup;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +11,10 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrdersGroupRequestDTO {
+    @PositiveOrZero(message = "Quantidade de itens não pode ser negativa")
     private Integer itemQuantity;
+    private String status;
     private String docUrl;
+    @Size(min = 1, message = "O grupo deve possuir no mínimo um pedido")
     private List<Long> ordersIds = new ArrayList<>();
 }
