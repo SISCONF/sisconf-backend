40aa1e5faf8a859fe9da603cd50d079d7a8849b6
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-14 17:46:55 -0300

    :wastebasket: remove: excludes unused import

--- src/main/java/br/ifrn/edu/sisconf/service/rabbitmq/TaskSender.java
+++ src/main/java/br/ifrn/edu/sisconf/service/rabbitmq/TaskSender.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.extern.slf4j.Slf4j;
 
