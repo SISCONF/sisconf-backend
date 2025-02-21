b7287e469718ea6888d8b74f58a1d981e5e448cd
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-21 17:25:02 -0300

    test mocking rabbitmq

--- src/test/java/br/ifrn/edu/sisconf/SecurityTestConfig.java
+++ src/test/java/br/ifrn/edu/sisconf/SecurityTestConfig.java
@@ -2,9 +2,12 @@ package br.ifrn.edu.sisconf;
 
 import static org.mockito.Mockito.mock;
 
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @TestConfiguration
 public class SecurityTestConfig {
@@ -12,4 +15,10 @@ public class SecurityTestConfig {
     public JwtDecoder jwtDecoder() {
         return mock(JwtDecoder.class);
     }
+
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
+
+    @MockitoBean
+    private RabbitAdmin rabbitAdmin;
 }
