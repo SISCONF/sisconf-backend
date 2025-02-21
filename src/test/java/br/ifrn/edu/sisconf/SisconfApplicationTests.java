b5aa272cab99bb11d199d56c059edec8f9374877
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-21 17:46:57 -0300

    testing again

--- src/test/java/br/ifrn/edu/sisconf/SisconfApplicationTests.java
+++ src/test/java/br/ifrn/edu/sisconf/SisconfApplicationTests.java
@@ -3,7 +3,7 @@ package br.ifrn.edu.sisconf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SisconfApplication.class)
+@SpringBootTest
 class SisconfApplicationTests {
 
 	@Test
