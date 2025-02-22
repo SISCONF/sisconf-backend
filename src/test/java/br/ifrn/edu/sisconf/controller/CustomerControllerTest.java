eff89ad30a8cf562845b61052c31988137ca4ff2
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-22 12:16:29 -0300

    tests: making sure all entrepreneurs get deleted

--- src/test/java/br/ifrn/edu/sisconf/controller/CustomerControllerTest.java
+++ src/test/java/br/ifrn/edu/sisconf/controller/CustomerControllerTest.java
@@ -120,6 +120,7 @@ public class CustomerControllerTest {
     @AfterEach
     public void tearDown() {
         customerRepository.deleteAll();
+        entrepreneurRepository.deleteAll();
     }
 
     @Test
